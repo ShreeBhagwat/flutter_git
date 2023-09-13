a0a0883be7293bbf253c8c7fa187556a040631f3
Shree Bhagwat <shree@codeaamy.com>
2023-09-13 16:23:03 +0530

    added all files together

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class CounterApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: CounterScreen()
     );
   }
